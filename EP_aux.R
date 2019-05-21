76b0fcdc224011c769b65b5bfe084f24c75bf55c
Fabricio Kassardjian <fkay13@gmail.com>
2019-05-20 21:06:54 -0300

    Teste com a distribuicao, exponencial tem potencial para aderência

--- EP_aux.R
+++ EP_aux.R
@@ -148,27 +148,35 @@ testeAmostras <- function(n, samples, p, silent = FALSE) {
   return(vals)
 }
 
-fit_dist <- function(n,rep,p,distribuicao){
-  entra = 1
-  # para cada distribuicao faz rep testes
-  for(i in 1:rep) {
-   
-    A = generateMatrix(n, p)
-    for(v in 1:n) {
-      T = findPath(v,v,A,0,n)
-      if(entra)
-      {
-        if(T != 1) 
-          c = T
-        entra=0
-      }
-      
-      if (T != 1 ) 
-        c = append(c,T)
-    }
-  }
+fit_dist <- function(n,repeticao,p,distribuicao){
+  # entra = 1
+  # # para cada distribuicao faz rep testes
+  # for(i in 1:repeticao) {
+  #  
+  #   A = generateMatrix(n, p)
+  #   for(v in 1:n) {
+  #     T = findPath(v,v,A,0,n)
+  #     if(entra)
+  #     {
+  #       if(T != 1) 
+  #         c = T
+  #       entra=0
+  #     }
+  #     
+  #     if (T != 1 ) 
+  #       c = append(c,T)
+  #   }
+  # }
+  d = testeSampleT(n, sampleSz = repeticao, p)
+  c = rep(1,d$distTAcum[3])
+  for(i in 4:n) 
+    c = append(c, rep(i-2,d$distTAcum[i]))
+  
+  print(d$distTAcum[3:n]/(n*repeticao - d$distTAcum[1] - d$distTAcum[n+1]))
+    
   library("fitdistrplus")
   fw <- fitdist(c,distribuicao) #faz com a distribuicao poisson
   summary(fw)
-  #plot(fw)
+  plot(fw)
+  return(fw)
 }
